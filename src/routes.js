e880aa1ab59bd4e16dcfbdc6ae5732500f4ba6a4
Janderson <jandersonpizziallyvilasboas@gmail.com>
2019-09-27 15:56:34 -0300

    chore: Incremento o aplicação MeetApp

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,23 @@
 import { Router } from 'express';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
-routes.post('/users', UserController.store);
-routes.post('/sessions', SessionController.store);
+routes.post('/users', UserController.store); // Rota de criação de um usuário
+routes.put('/users', UserController.update); // Rota de update de um usuário
+routes.post('/sessions', SessionController.store); // Rota de Autenticação de usuário
 
-routes.use(authMiddleware);
+routes.use(authMiddleware); // Middleware Global
 
-routes.put('/users', UserController.update);
+routes.post('/files', upload.single('file'), FileController.store); // Rota de add um arquivo para avatar
 
 export default routes;
